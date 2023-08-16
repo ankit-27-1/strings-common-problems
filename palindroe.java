2dc504e2d8fa3c0c267c6c5d6bab2c3d64601323
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-08-16 22:22:13 +0530

    Create palindrome.java
    check if a string is palindrome

---
+++ palindroe.java
@@ -0,0 +1,15 @@
+public class Main {
+    static boolean isPalindrome(String str){
+        int n=str.length();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)!=str.charAt(n-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str="racecar";
+        System.out.println(isPalindrome(str));
+    }
+}
