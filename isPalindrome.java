3653d09e867f3e523ec8f5968a563faf010842a8
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-08-20 00:00:21 +0530

    Create isPalindrome.java
    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
    
    Given a string s, return true if it is a palindrome, or false otherwise.

---
+++ isPalindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start=0;
+        int end=s.length()-1;
+        while(start<=end){
+            char currFirst=s.charAt(start);
+            char currLast=s.charAt(end);
+            if(!Character.isLetterOrDigit(currFirst)){
+                start++;
+            }
+            else if(!Character.isLetterOrDigit(currLast)){
+                end--;
+            }
+            else{
+                if(Character.toLowerCase(currFirst)!=Character.toLowerCase(currLast)){
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
